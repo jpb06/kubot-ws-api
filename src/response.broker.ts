4263966ccbcbee7ce17864797e8dd1b36c26bd14
jpb <jpb.06@outlook.com>
2018-09-22 10:10:40 +0200

    Factorizing input validation and output generation

---
+++ src/response.broker.ts
@@ -0,0 +1,18 @@
+﻿import { Response } from "express-serve-static-core";
+
+export abstract class ResponseBroker {
+    public static Reply(data: any, res: Response): Response {
+        if (data === undefined) {
+            return res.status(404).json({
+                status: 'Not found',
+                data: null
+            });
+        }
+        else {
+            return res.status(200).json({
+                status: 'Success',
+                data: data
+            });
+        }
+    }
+}
