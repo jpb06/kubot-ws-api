99a472870bb9c9a63305b81e981cbaeb7f940699
jpb <jpb.06@outlook.com>
2018-10-02 18:58:38 +0200

    Implementing extends and moving guild/website routes to their own files for clarity

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,57 @@
 ﻿import debug = require('debug');
 import express = require('express');
-
+import { Express, Response } from "express-serve-static-core";
 import * as bodyParser from "body-parser"; // pull information from HTML POST (express4)
-import { InputValidator } from "./input.validator";
-import { ResponseBroker } from "./response.broker";
 
-import * as Dal from 'kubot-dal';
-Dal.Configuration.Setup('mongodb://127.0.0.1:27017', 'kubot-ts');
+import { Configuration as KubotDalConfiguration } from 'kubot-dal';
+import { Configuration as RsaStoreConfiguration } from 'rsa-store';
+KubotDalConfiguration.Setup('mongodb://127.0.0.1:27017', 'kubot-ts');
+RsaStoreConfiguration.Setup('127.0.0.1:27017', 'cryptography-db');
 
-var app = express();
+import { GuildRoutes } from './routes/guild.routes';
+import { WebsiteRoutes } from './routes/website.routes';
+
+let app: Express = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:4200");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.populate = function (data: any): Response {
+        if (data === undefined) {
+            return res.status(404).json({
+                status: 'Not found',
+                data: null
+            });
+        } else {
+            return res.status(200).json({
+                status: 'Success',
+                data: data
+            });
+        }
+    }
+    res.badRequest = function (message: string): Response {
+        return res.status(400).json({
+            status: message,
+            data: null
+        });
+    }
+    req.validateId = function (): boolean {
+        if (req.body.id === undefined || req.body.id === '') {
+            return false;
+        }
+
+        return true;
+    }
+    req.validateLogin = function (): boolean {
+        if ((req.body.login === undefined || req.body.login === '') ||
+            (req.body.password === undefined || req.body.password === '')) {
+            return false;
+        }
+
+        return true;
+    }
     next();
 });
 
@@ -24,27 +61,13 @@ app.get('/', (req, res) => {
 app.get('/api/', (req, res) => {
     res.send('This is Kubot-ws API.');
 });
-app.post('/api/guild', async (req, res) => {
-    InputValidator.validate(req.body.id, res);
-
-    let guild = await Dal.Manipulation.GuildsStore.get(req.body.id);
-    return ResponseBroker.Reply(guild, res);
-});
-app.post('/api/regions', async (req, res) => {
-    InputValidator.validate(req.body.id, res);
-
-    let regions = await Dal.Manipulation.RegionWatchStore.get(req.body.id);
-    return ResponseBroker.Reply(regions, res);
-});
-app.post('/api/factions', async (req, res) => {
-    InputValidator.validate(req.body.id, res);
-
-    let factions = await Dal.Manipulation.FactionWatchStore.get(req.body.id);
-    return ResponseBroker.Reply(factions, res);
-});
+GuildRoutes.Map(app);
+WebsiteRoutes.Map(app);
 
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
 });
+
+let a = 0;
