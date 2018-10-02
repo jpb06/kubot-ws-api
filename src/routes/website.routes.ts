83d15ce90866037ad57526b7c9735e2b05c39a6e
jpb <jpb.06@outlook.com>
2018-10-02 18:59:18 +0200

    Website routes (Only Login for now)

---
+++ src/routes/website.routes.ts
@@ -0,0 +1,44 @@
+﻿import { Express } from "express-serve-static-core";
+import * as jwt from 'jsonwebtoken';
+
+import * as Dal from 'kubot-dal';
+import * as RsaProvider from 'rsa-store';
+
+import { KeyPair } from "rsa-store/typings/types.export";
+
+export abstract class WebsiteRoutes {
+
+    public static Map(app: Express) {
+        app.post('/api/ws/login', async (req, res) => {
+            try {
+                if (!req.validateLogin()) {
+                    return res.badRequest('Expecting identifiers');
+                }
+
+                let user = await Dal.Manipulation.SessionStore.get(req.body.login);
+                if (user != null && user.password === req.body.password) {
+                    let keyPair: KeyPair = await RsaProvider.CryptoService.GetKeyPair('kubot-ws');
+
+                    const jwtBearerToken = jwt.sign({}, keyPair.privateKey, {
+                        algorithm: 'RS256',
+                        expiresIn: 120,
+                        subject: user.token
+                    });
+
+                    return res.status(200).json({
+                        status: 'Success',
+                        token: jwtBearerToken,
+                        expiresIn: 120
+                    });
+                } else {
+                    return res.status(401).json({
+                        status: 'Not authorized',
+                        data: null
+                    });
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        });
+    }
+}
