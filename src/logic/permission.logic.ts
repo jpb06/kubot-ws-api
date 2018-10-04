e1b21127e2a0887bc132111c194905383236d4f0
jpb <jpb.06@outlook.com>
2018-10-04 19:18:55 +0200

    Permissions request handler

---
+++ src/logic/permission.logic.ts
@@ -0,0 +1,14 @@
+﻿import * as expressJwt from 'express-jwt';
+import { KeyPair } from 'rsa-store/typings/types.export';
+import { CryptoService } from 'rsa-store';
+
+export abstract class PermissionLogic {
+
+    public static async IsAuthenticated(): Promise<expressJwt.RequestHandler> {
+        let keyPair: KeyPair = await CryptoService.GetKeyPair('kubot-ws');
+
+        return expressJwt({
+            secret: keyPair.publicKey
+        });
+    } 
+}
