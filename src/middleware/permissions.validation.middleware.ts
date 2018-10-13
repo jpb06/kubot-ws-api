2b9bda39315872fb6c4281d3c06dfb6accd72b54
jpb <jpb.06@outlook.com>
2018-10-13 13:13:36 +0200

    Setting up proper authorization validation

---
+++ src/middleware/permissions.validation.middleware.ts
@@ -0,0 +1,45 @@
+﻿import * as jwt from 'jsonwebtoken';
+import { KeyPair } from 'rsa-store/typings/types.export';
+import { CryptoService } from 'rsa-store';
+import { Request, Response, NextFunction } from 'express';
+
+export async function isAuthenticated(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    try {
+        let authorizationHeaders = req.headers.authorization || '';
+        let chunks = authorizationHeaders.split(' ');
+
+        if (chunks.length === 0 || chunks[0] !== 'Bearer' || chunks[1].length === 0) {
+            return res.status(401).json({
+                status: 401,
+                message: 'Not logged in'
+            });
+        }
+
+        let keyPair: KeyPair = await CryptoService.GetKeyPair('kubot-ws');
+
+        jwt.verify(chunks[1], keyPair.publicKey);
+
+        next();
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({
+                status: 401,
+                message: 'Token has expired'
+            });
+        } else if (error.name === 'JsonWebTokenError' && error.message.startsWith('jwt subject invalid')) {
+            return res.status(401).json({
+                status: 401,
+                message: 'Invalid token'
+            });
+        } else {
+            return res.status(500).json({
+                status: 500,
+                message: error.message
+            });
+        }
+    }
+};
