5432d359270cbda1b0ecc11a019e20a0a70b50f0
jpb <jpb.06@outlook.com>
2018-12-09 08:50:21 +0100

    Implementing a middleware to verify roles

--- src/middleware/permissions.validation.middleware.ts
+++ src/middleware/permissions.validation.middleware.ts
@@ -2,6 +2,22 @@
 import { KeyPair } from 'rsa-provider/typings/types.export';
 import { CryptoService } from 'rsa-provider';
 import { Request, Response, NextFunction } from 'express';
+import * as Dal from 'kubot-dal';
+import { RequestHandlerParams } from 'express-serve-static-core';
+
+function verifyHeaders(
+    req: Request,
+    res: Response
+): string {
+    let authorizationHeaders = req.headers.authorization || '';
+    let chunks = authorizationHeaders.split(' ');
+
+    if (chunks.length === 0 || chunks[0] !== 'Bearer' || chunks[1].length === 0) {
+        return '';
+    }
+
+    return chunks[1];
+}
 
 export async function isAuthenticated(
     req: Request,
@@ -9,10 +25,8 @@ export async function isAuthenticated(
     next: NextFunction
 ) {
     try {
-        let authorizationHeaders = req.headers.authorization || '';
-        let chunks = authorizationHeaders.split(' ');
-
-        if (chunks.length === 0 || chunks[0] !== 'Bearer' || chunks[1].length === 0) {
+        let token = verifyHeaders(req, res);
+        if (token === '') {
             return res.status(401).json({
                 status: 401,
                 message: 'Not logged in'
@@ -21,7 +35,8 @@ export async function isAuthenticated(
 
         let keyPair: KeyPair = await CryptoService.GetKeyPair('kubot-ws');
 
-        jwt.verify(chunks[1], keyPair.publicKey);
+        let result = jwt.verify(token, keyPair.publicKey);
+        res.locals.login = (<any>result).guild;
 
         next();
     } catch (error) {
@@ -43,3 +58,39 @@ export async function isAuthenticated(
         }
     }
 };
+
+export function HasRole(
+    role: string
+): RequestHandlerParams {
+    return async function (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<Response | undefined> {
+        try {
+            if (res.locals.login === null) {
+                return sendNotAuthorizedResponse(res);
+            }
+
+            let roles = await Dal.Manipulation.SessionStore.getPermissions(res.locals.login);
+
+            if ((roles === null || !Array.isArray(roles)) || roles.indexOf(role) === -1) {
+                return sendNotAuthorizedResponse(res);
+            }
+
+            next();
+        } catch (error) {
+            return res.status(500).json({
+                status: 500,
+                message: error.message
+            });
+        }
+    }
+}
+
+function sendNotAuthorizedResponse(res: Response): Response {
+    return res.status(401).json({
+        status: 401,
+        message: 'Not authorized'
+    });
+}
