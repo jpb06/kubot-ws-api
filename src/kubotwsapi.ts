d0395f306199e1df88558e4e664cdd3567164005
jpb <jpb.06@outlook.com>
2018-12-27 19:09:11 +0100

    Setting port to 3000

--- src/kubotwsapi.ts
+++ src/kubotwsapi.ts
@@ -36,7 +36,7 @@ mapGuildRoutes(app);
 mapSecurityRoutes(app);
 mapStaticRoutes(app);
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', 3000);
 
 var server = app.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
